fd7c1cefcb0203351f7e972d8f209328559178c1
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-03 19:19:38 +0200

    Refactored Code to fix Output on number 15

--- julia-james-game.js
+++ julia-james-game.js
@@ -17,12 +17,20 @@ while (x <= 20) {
     // check divisibility
     // print Julia, James, or JuliaJames
 
-    if (x % 3 === 0){
+    
+
+    if ((x % 3 === 0) && (x % 5 === 0)){
+        console.log("JuliaJames");
+
+
+    } else if(x % 3 === 0){
         console.log("Julia");
+
+
     } else if(x % 5 === 0){
         console.log("James");
-    } else if((x % 3 === 0) && (x % 5 === 0)){
-        console.log("JuliaJames");
+
+
     }else {
         console.log(x);
     }
