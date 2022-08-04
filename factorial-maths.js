d060c7485bbe891d2504e6f4401c48a0b5c118fe
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-04 16:24:28 +0200

    using for loop for factorial maths

---
+++ factorial-maths.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Factorials
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your code should produce the expected output
+ */
+
+// your code goes here
+
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+
+}
+
+console.log(solution);
+
