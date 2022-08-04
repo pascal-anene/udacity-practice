ba9560fddb6f65923184f8ade7df94517ac6d564
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-04 13:19:29 +0200

    convert while loop to for loop

---
+++ change-loop.js
@@ -0,0 +1,15 @@
+/**
+ * Rewriting a While Loop as For Loop
+ * Rewrite the following while loop as a for loop:
+ * let x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+}
+ */
+
+
+for(let x = 9; x >= 1; x--){
+    console.log("hello " + x);
+}
+
