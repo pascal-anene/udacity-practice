e46a73ca1b45c7eeed90b34a831d918d7545ae26
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-05 19:25:16 +0200

    starter logic to output the cry of any living thing

---
+++ cry-sound.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz: Cry
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `cry`
+ * - Your code should include a named function expression stored in the variable `cry`
+ * - Your code should call the function expression using the variable name, not the function name
+ * - Your function expression should return the expected output
+ */
+
+
+// your code goes here
+let cry = function crying(){
+    return "boohoo!";
+}
+
+
+// test your solution
+console.log(cry());
