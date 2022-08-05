661267ff99cef607b99ea05fd73f718f1074e235
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-05 19:22:01 +0200

    refactored code using anonymous function expression

--- laugh-function.js
+++ laugh-function.js
@@ -7,12 +7,25 @@
  * - Your code should have a `laugh()` function
  * - Your `laugh()` function should return the correct output
  * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()`
+ *
+ * 
+ * * REFACTORING REQUIREMENTS
+ * - Your code should have a variable `laugh`
+ * - Your code should include an anonymous function expression stored in the variable `laugh`
+ * - Your anonymous function expression should take one argument
+ * - Your anonymous function expression should return the correct number of `hahaha`\'s
  */
+
+
+
 
 // your code goes here
 
 // function declaration with parameter
-function laugh(num){
+
+//change the functon to an anonymous functon by storing in a variable called laugh
+
+let laugh = function(num){
 
     // variable to build the number of ha's
     let laughter = "";
@@ -30,10 +43,16 @@ function laugh(num){
 
     return finalSound;
 
-}
+};
 
 // test code
 console.log(laugh(0)) 
 console.log(laugh(3)) 
 console.log(laugh(4)) 
 console.log(laugh(8)) 
+
+// laugh(0);
+// laugh(3);
+// laugh(4);
+// laugh(8);
+
