6284f9354d1afaec9e9d330692d81e6c63986edb
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-05 12:19:17 +0200

    practicing function decleration and call

---
+++ laugh-function.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Laugh it Off 1
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()`
+ */
+
+// your code goes here
+function laugh(){
+
+    let laughter = "hahahahahahahahahaha!"
+
+    return laughter;
+
+}
+
+// test your code by logging out the returned value
+console.log(laugh());
