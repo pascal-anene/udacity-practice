7fe2f1996d7572e79029702bc348394f7ac3720e
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-06 07:51:41 +0200

    family array list with literal syntax

---
+++ family-array.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `udaciFamily`
+ * - The variable `udaciFamily` should be an array containing the values `"Julia"`, `"James"`, and one other name
+ * - Your code should print `udaciFamily` to the console
+ */
+
+
+// your code goes here
+
+//Declaration using an array literal syntax
+const udaciFamily = ["Julia","James","Pascal"];
+
+
+for(let index = 0; index < udaciFamily.length; index++){
+
+    let familyMember = "";
+
+    familyMember = udaciFamily[index];
+
+    console.log(familyMember);
+
+}
+
