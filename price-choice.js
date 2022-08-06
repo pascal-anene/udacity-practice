cb16f49062024a38a0010184eb837ec7a27569da
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-06 08:35:07 +0200

    refactoring to iterate using for loop

--- price-choice.js
+++ price-choice.js
@@ -11,11 +11,35 @@
 const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
 
 // your code goes here
-prices[0] = (prices[0] * 2);
+// prices[0] = (prices[0] * 2);
 
-prices[2] = (prices[2] *2);
+// prices[2] = (prices[2] *2);
 
-prices[6] = (prices[6] * 2);
+// prices[6] = (prices[6] * 2);
+
+// console.log(prices);
+
+/**
+ * The solution above can be implemented using loop iteration
+ */
+
+for(let index = 0; index < prices.length; index++){
+
+    //Since we have conditions to double certain elements, we use conditionals
+    if(index === 0){
+
+        prices[0] = prices[0] * 2;
+
+    }else if (index === 2){
+
+        prices[2] = prices[2] * 2;
+
+    }else if(index === 6){
+
+        prices[6] = prices[6] * 2;
+
+    }
+
+}
 
 console.log(prices);
-
