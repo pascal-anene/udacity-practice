132c8fb63dda44a1acaefa3b6da650c479f47f9e
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-06 08:03:45 +0200

    printing movie characters in arrays

---
+++ serenity-movie.js
@@ -0,0 +1,45 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `crew`
+ * - The variable `crew` should be an array containing the Serenity\'s crew
+ * - Your code should print `crew` to the console as an array. Do not iterate over the elements. 
+ * 
+    const captain = "Mal";
+    const second = "Zoe";
+    const pilot = "Wash";
+    const companion = "Inara";
+    const mercenary = "Jayne";
+    const mechanic = "Kaylee";
+ * 
+ */
+ 
+ 
+ 
+
+// your code goes here
+
+//Array declaration using literal syntax 
+
+ const crew = [
+            "Mal",
+            "Zoe",
+            "Wash",
+            "Inara",
+            "Jayne",
+            "Kaylee"]
+
+//Array declaration using constructor syntax 
+
+const crewSame = new Array(
+                "Mal",
+                "Zoe",
+                "Wash",
+                "Inara",
+                "Jayne",
+                "Kaylee"
+);
+
