91bc66edb60ca45e7f1396cfc0076789aed387bf
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-06 18:48:07 +0200

    managing account balance using object literals

---
+++ bank-account.js
@@ -0,0 +1,39 @@
+/*
+ * Programming Quiz: Bank Accounts 1
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `savingsAccount` 
+ * - Your `savingsAccount` object should have the `balance` and `interestRatePercent` property
+ * - Your `savingsAccount` object should have a `printAccountSummary()` method
+ * - Your `printAccountSummary()` method should return the EXACT expected message.
+  * - Be careful about the punctuation, spacing, and spelling of the printed message.
+ */
+
+
+const savingsAccount = {
+
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    // your code goes here
+    printAccountSummary: function printSummary(){
+        return `Welcome!\n Your balance is currently $${savingsAccount.balance} and your interest rate is ${savingsAccount.interestRatePercent}%.`;
+    }
+
+};
+
+console.log(savingsAccount.printAccountSummary());
+
+
