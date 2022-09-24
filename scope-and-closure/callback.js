a8d6e6a344a6955acfb269df0361d382031bd3bd
Pascal Anene <pascal.o.anene@gmail.com>
2022-09-24 15:34:03 +0200

    more practice on array method using filter

--- scope-and-closure/callback.js
+++ scope-and-closure/callback.js
@@ -9,6 +9,47 @@
  *   - Do not delete the musicData variable
  *   - Do not alter any of the musicData content
  *   - Do not format the sales number; leave it as a long string of digits
+ *   - After practice comment out the code in preparation for next practice
+ */
+
+// const musicData = [
+//     { artist: 'Adele', name: '25', sales: 1731000 },
+//     { artist: 'Drake', name: 'Views', sales: 1608000 },
+//     { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+//     { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+//     { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+//     { artist: 'Original Broadway Cast Recording', 
+//       name: 'Hamilton: An American Musical', sales: 820000 },
+//     { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+//     { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+//     { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+//     { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+// ];
+
+// const albumSalesStrings = musicData.map(function(item){
+//             return `${item.name} by ${item.artist}  sold ${item.sales} copies`
+// });
+
+// console.log(albumSalesStrings);
+
+
+// for(let i = 0; i < albumSalesStrings.length; i++){
+//     console.log(albumSalesStrings[i]);
+// }
+
+
+
+
+/* Using filter()
+ *
+ * Using the musicData array and filter():
+ *   - Return only album objects where the album's name is
+ *     10 characters long, 25 characters long, or anywhere in between
+ *   - Store the returned data in a new `results` variable
+ *
+ * Note:
+ *   - Do not delete the musicData variable
+ *   - Do not alter any of the musicData content
  */
 
 const musicData = [
@@ -25,17 +66,13 @@ const musicData = [
     { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
 ];
 
-const albumSalesStrings = musicData.map(function(item){
-            return `${item.name} by ${item.artist}  sold ${item.sales} copies`
+const results = musicData.filter(function(item){
+    if( (item.name.length >= 10) && (item.name.length <= 25)){
+        return item;
+    }
 });
 
-console.log(albumSalesStrings);
 
 
-for(let i = 0; i < albumSalesStrings.length; i++){
-    console.log(albumSalesStrings[i]);
-}
-
-
-
+console.log(results);
 
